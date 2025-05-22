a64382c6cf478272ab478a7367b00621bbe74f37
Safal Bhandari <108601244+SafalBhandari12@users.noreply.github.com>
2025-05-22 20:20:34 +0545

    Removed password required field

--- models/User.ts
+++ models/User.ts
@@ -6,13 +6,11 @@ import jwt from "jsonwebtoken";
 export interface IUser extends Document {
   name: string;
   mobile: string;
-  password: string;
   role: "user" | "vendor" | "admin";
   isVerified: boolean;
   otp?: string;
   otpExpires?: Date;
   address?: string;
-  comparePassword(candidatePassword: string): Promise<boolean>;
   generateAuthToken(): string;
 }
 
@@ -20,7 +18,6 @@ const UserSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     mobile: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
     role: {
       type: String,
       enum: ["user", "vendor", "admin"],
@@ -36,26 +33,6 @@ const UserSchema = new Schema<IUser>(
 
 // Create indexes
 UserSchema.index({ mobile: 1 }, { unique: true });
-
-// Hash password before saving
-UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error as Error);
-  }
-});
-
-// Compare password method
-UserSchema.methods.comparePassword = async function (
-  candidatePassword: string
-): Promise<boolean> {
-  return bcrypt.compare(candidatePassword, this.password);
-};
 
 // Generate JWT token
 UserSchema.methods.generateAuthToken = function (): string {
